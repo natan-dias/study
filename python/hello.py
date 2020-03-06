4c75dc28b351c04bfcaaaf3bc527a5decdd7900a
Nataniel Dias de Almeida <natanieldias@tjrj.jus.br>
2020-03-06 20:47:05 -0300

    python first commit

---
+++ python/hello.py
@@ -0,0 +1,70 @@
+import subprocess
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+from tkinter import ttk
+
+#root= tk.Tk()
+window = Tk()
+window.title("Atualização SAR")
+window.geometry('600x600')
+
+selected = StringVar()
+
+lbl = Label(window, text="")
+lbl.grid(column=1, row=2, columnspan=4, sticky=W)
+#txt = Entry(window, width=10)
+#txt.grid(column=1, row=3)
+#txt.focus() #Focus txt area
+
+rad1 = Radiobutton(window,text='tjerj230avm', value="tjerj230avm", variable=selected)
+rad2 = Radiobutton(window,text='tjerj230bvm', value="tjerj230bvm", variable=selected)
+rad3 = Radiobutton(window,text='tjerj230cvm', value="tjerj230cvm", variable=selected)
+
+def clicked():
+    #print(selected.get())
+    #res = "Servidor " + txt.get()
+    lbl.configure(text= "Servidor selecionado: " + selected.get())
+    ttk.Separator(window, orient=HORIZONTAL).grid(row=5, columnspan=5, sticky=EW)
+    messagebox.showinfo('Título', 'Servidor selecionado: ' + selected.get())
+    
+    
+
+
+
+btn = Button(window, text="Select Server", command=clicked)
+rad1.grid(column=1, row=0)
+rad2.grid(column=2, row=0)
+rad3.grid(column=3, row=0)
+btn.grid(column=4, row=0)
+
+
+#combo = Combobox(window)
+#combo['values']= (1, 2, 3, 4, 5, "Text")
+#combo.current(1) #Set selected item
+#combo.grid(column=1, row=4)
+
+#canvas1 = tk.Canvas(window, width = 350, height = 250)
+#canvas1.pack()
+
+def start_batch():
+    subprocess.call([r'.\teste.bat'])
+
+#button1 = Button (window, text='Run ping',command=start_batch)
+#canvas1.create_window(170, 130, window=button1)
+
+#print("Hello World")
+
+#Warning Messages
+"""
+res = messagebox.askquestion('Message title','Message content')
+
+res = messagebox.askyesno('Message title','Message content')
+
+res = messagebox.askyesnocancel('Message title','Message content')
+
+res = messagebox.askokcancel('Message title','Message content')
+
+res = messagebox.askretrycancel('Message title','Message content')
+"""
+
